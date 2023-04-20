f7d5efb347d17e1efbb95caf6f2002636fa3b7b3
Cskorpion <32685843+Cskorpion@users.noreply.github.com>
2023-04-20 11:15:37 +0200

    stack- and stringtable with tests
    from yesturday

--- vmprofconvert/__init__.py
+++ vmprofconvert/__init__.py
@@ -2,4 +2,41 @@ import vmprof
 
 def convert(path):
     stats = vmprof.read_profile(path)
+    c = Converter()
+    for sample in stats.profiles:
+        pass
     return stats
+
+class Converter:
+    def __init__(self):
+        self.stringtable = []
+        self.stringtable_positions = {}
+        self.stacktable = [] #list of [frameindex, stacktableindex_or_None]
+        self.stacktable_positions = {}
+
+    def add_string(self, string):
+        if string in self.stringtable_positions:
+            return self.stringtable_positions[string]
+        else:
+            result = len(self.stringtable)
+            self.stringtable.append(string)
+            self.stringtable_positions[string] = result
+            return result
+    
+    def add_stack(self, stack):
+        #stack is a list of frametable indexes
+        if not stack:
+            return None
+        else:
+            top = stack[-1]
+            rest = stack[:-1]
+            rest_index = self.add_stack(rest)
+            key = (top, rest_index)
+            if key in self.stacktable_positions:
+                return self.stacktable_positions[key]
+            else:
+                result = len(self.stacktable)
+                self.stacktable.append([top, rest_index])
+                self.stacktable_positions[key] = result
+                return result
+    
--- vmprofconvert/test/test_convert.py
+++ vmprofconvert/test/test_convert.py
@@ -1,9 +1,28 @@
 import os
 from vmprofconvert import convert
+from vmprofconvert import Converter
 
 def test_example():
     path = os.path.join(os.path.dirname(__file__), "example.prof")
     result = convert(path)
     
+def test_stringtable():
+    c = Converter()
+    index = c.add_string("Hallo")
+    assert index == 0
+    index = c.add_string("Hallo")
+    assert index == 0
+    index = c.add_string("Huhu")
+    assert index == 1
+    assert c.stringtable == ["Hallo", "Huhu"]
 
+def test_stacktable():
+    c = Converter()
+    assert c.add_stack([]) is None
+    stackindex0 = c.add_stack([1,2,3])# Top of Stack is 3
+    stackindex1 = c.add_stack([1,2,3])
+    assert stackindex0 == stackindex1 == 2
+    assert c.stacktable == [[1,None], [2,0], [3,1]]
+    stackindex2 = c.add_stack([1,2,3,4])
+    assert stackindex2 == stackindex1 + 1
 
